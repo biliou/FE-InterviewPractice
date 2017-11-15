8474880b0712497413d1fc105f79065bf5ea6f43
bili.ou <bili.ou@cyphertech.cn>
2017-11-15 18:00:44 +0800

    添加es6 demo

---
+++ javaScriptLesson/es6/module/console.js
@@ -0,0 +1,13 @@
+//function console() {
+//	console.log(222)
+//}
+//
+//export {
+//	console
+//};
+
+var firstName = 'Michael';
+var lastName = 'Jackson';
+var year = 1958;
+
+export {firstName, lastName, year};
