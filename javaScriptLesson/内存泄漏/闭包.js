7996aec1c3fa814e8d870cd6fe7fa467c0ed2f45
vicki <644495757@qq.com>
2018-03-18 14:54:08 +0800

    更新闭包例子

--- javaScriptLesson/内存泄漏/闭包.js
+++ javaScriptLesson/内存泄漏/闭包.js
@@ -2,12 +2,17 @@ function numberGenerator(){
 	var num = 1;
 	
 	function checkNumber(){
-		console.log(num);
+		return num;
 	}
+	
+//	var checkNumber = function (){  //函数表达式
+//		return num;
+//	}
 	
 	num++;
 	return checkNumber;
 }
 
-var number = numberGenerator();
-number(); //2
+var fn = numberGenerator();
+var number = fn(); 
+consoe.log(number); //2
